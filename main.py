44a5c641eae3d4aa87dff4f195fe8cb6cfd4d7dc
RandomLambda <34924627+RandomLambda@users.noreply.github.com>
2022-11-12 12:56:12 +0100

    classifing glass colleting

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from read_data import from_csv_with_coordinates
+from read_data import from_csv_glas
 
-unfall = from_csv_with_coordinates("strassenverkehrsunfallorte.csv")
-robidog = from_csv_with_coordinates("hundekotbehalter.csv")
+#unfall = from_csv_with_coordinates("strassenverkehrsunfallorte.csv")
+#robidog = from_csv_with_coordinates("hundekotbehalter.csv")
+glas = from_csv_glas("fullstandssensoren-sammelstellen-stadt-stgallen.csv")
 
 
 
--- read_data.py
+++ read_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 
-def from_csv_with_coordinates(filename):
+def from_csv_with_coordinates(filename): #, **kwargs
 
 	#with open(filename, 'r') as f:
 	#	data = f.readlines()
@@ -35,6 +35,46 @@ def from_csv_with_coordinates(filename):
 	return df
 		
 
+def from_csv_glas(filename): #, **kwargs
+
+	#with open(filename, 'r') as f:
+	#	data = f.readlines()
+		
+
+	with open(filename, 'r') as f:
+		data = pd.read_csv(f, sep=';')
+		
+	header = list(data) #data.pop(0)
+	
+	print(data)
+
+	if 'geo_point_2d' in header:
+		#i_loc = header.index('Location')
+		
+		#df['loc'] = data['Location']
+		df = pd.DataFrame()
+		df[['lon', 'lat']] = data['geo_point_2d'].str.split(',', 1, expand=True)
+		df["braun"] = [1 if "Braun" in ele else 0 for ele in data["Tags"]]
+		df["green"] = [1 if "GrÃ¼n" in ele else 0 for ele in data["Tags"]]
+		df["white"] = [1 if "Weiss" in ele else 0 for ele in data["Tags"]]
+
+		df['value'] = 1
+		
+	elif 'Geo Point' in header:
+		df = pd.DataFrame()
+		df[['lon', 'lat']] = data['Geo Point'].str.split(',', 1, expand=True)
+		df['value'] = 1
+		
+	else:
+		raise ValueError('no Location found in dataset')
+		
+	
+	print(df)
+	
+	return df
+		
+
+	
 	
 	
 	
